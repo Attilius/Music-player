6e4c5f13002c454d1f135fc2688b5b200bbfc9ce
Attilius <leo20@freemail.hu>
2021-07-06 20:24:26 +0200

    Remove className from ul tag of menu component

--- music-player-app/src/Menu.js
+++ music-player-app/src/Menu.js
@@ -2,7 +2,7 @@ function Menu() {
     return(
       <div className="head">
         <img class="logo" src="./pics/podcast-logo-simple-design_169533-99-removebg-preview.png" alt="logo"></img>
-        <ul className="nav justify-content-end">
+        <ul>
         <li className="nav-item">
           <a className="nav-link" href="/">Musics</a>
         </li>
