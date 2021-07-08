bba9d5bf9ff827d63d1ce1de0ec1147968a4e342
Attilius <leo20@freemail.hu>
2021-07-08 16:35:40 +0200

    I setup the control buttons working styles

--- music-player-app/src/ControlPanel.js
+++ music-player-app/src/ControlPanel.js
@@ -1,13 +1,30 @@
 
-function ControlPanel({ className }) {
+function ControlPanel({ className, playBtnClassName, backwardBtnClassName, 
+                        forwardBtnClassName, stopBtnClassName, pauseBtnClassName, onChange }) {
+    
     return (
         <div className={ className }>
                 <ul id="control-btns">
-                    <li><button id="play_" className="btn"><i className="fas fa-play"></i></button></li>
-                    <li><button id="backward_" className="btn"><i className="fas fa-step-backward"></i></button></li>
-                    <li><button id="forward_" className="btn"><i className="fas fa-step-forward"></i></button></li>
-                    <li><button id="stop_" className="btn"><i className="fas fa-stop"></i></button></li>
-                    <li><button id="pause_" className="btn"><i className="fas fa-pause"></i></button></li>
+                    <li><button id="play_" 
+                                className={ playBtnClassName }
+                                onClick={ onChange }>
+                                <i className="fas fa-play"></i></button></li>
+                    <li><button id="backward_" 
+                                className={ backwardBtnClassName }
+                                onClick={ onChange }>
+                                <i className="fas fa-step-backward"></i></button></li>
+                    <li><button id="forward_" 
+                                className={ forwardBtnClassName }
+                                onClick={ onChange }>
+                                <i className="fas fa-step-forward"></i></button></li>
+                    <li><button id="stop_" 
+                                className={ stopBtnClassName }
+                                onClick={ onChange }>
+                                <i className="fas fa-stop"></i></button></li>
+                    <li><button id="pause_" 
+                                className={ pauseBtnClassName }
+                                onClick={ onChange }>
+                                <i className="fas fa-pause"></i></button></li>
                 </ul>
         </div>
     )
--- music-player-app/src/MediaPlayer.js
+++ music-player-app/src/MediaPlayer.js
@@ -5,7 +5,12 @@ import PowerButton from './PowerButton';
 
 function MediaPlayer() {
     const [powerBtnClassName, setPowerBtnClassName] = useState("power-off");
-    const [controlBtnsClassName, setControlBtnsClassName] = useState("controlpanel");
+    const [controlBtnsClassName, setControlBtnsClassName] = useState("hidden");
+    const [playBtnClassName, setPlayBtnClassName] = useState("btn");
+    const [backwardBtnClassName, setBackwardBtnClassName] = useState("btn");
+    const [forwardBtnClassName, setForwardBtnClassName] = useState("btn");
+    const [stopBtnClassName, setStopBtnClassName] = useState("btn");
+    const [pauseBtnClassName, setPauseBtnClassName] = useState("btn");
 
     const handleChangePowerBtnClassList = () => {
         if (powerBtnClassName === "power-off") {
@@ -17,6 +22,51 @@ function MediaPlayer() {
         }
     }
 
+    const handleChangeControlBtnClassName = (e) => {
+
+        const btnId = e.nativeEvent.path[1].id;
+            switch (btnId) {
+                case "play_":
+                    setPlayBtnClassName("active");
+                    setBackwardBtnClassName("btn");
+                    setForwardBtnClassName("btn");
+                    setStopBtnClassName("btn");
+                    setPauseBtnClassName("btn");
+                    break;
+                case "backward_":
+                    setPlayBtnClassName("btn");
+                    setBackwardBtnClassName("active");
+                    setForwardBtnClassName("btn");
+                    setStopBtnClassName("btn");
+                    setPauseBtnClassName("btn");
+                    break;
+                case "forward_":
+                    setPlayBtnClassName("btn");
+                    setBackwardBtnClassName("btn");
+                    setForwardBtnClassName("active");
+                    setStopBtnClassName("btn");
+                    setPauseBtnClassName("btn");
+                    break;
+                case "stop_":
+                    setPlayBtnClassName("btn");
+                    setBackwardBtnClassName("btn");
+                    setForwardBtnClassName("btn");
+                    setStopBtnClassName("active");
+                    setPauseBtnClassName("btn");
+                    break;
+                case "pause_":
+                    setPlayBtnClassName("btn");
+                    setBackwardBtnClassName("btn");
+                    setForwardBtnClassName("btn");
+                    setStopBtnClassName("btn");
+                    setPauseBtnClassName("active");
+                    break;
+            
+                default:
+                    break;
+            }
+    }
+
     return(
         <div className="media-content">
             <div className="media-player-content">
@@ -24,7 +74,13 @@ function MediaPlayer() {
                     <YouTubeMediaBox />
                     <PowerButton  className={ powerBtnClassName } onChange={ handleChangePowerBtnClassList } />
                 </div>
-                <ControlPanel className={ controlBtnsClassName } />
+                <ControlPanel className={ controlBtnsClassName } 
+                              playBtnClassName={ playBtnClassName }
+                              backwardBtnClassName={ backwardBtnClassName }
+                              forwardBtnClassName={ forwardBtnClassName }
+                              stopBtnClassName={ stopBtnClassName }
+                              pauseBtnClassName={ pauseBtnClassName }
+                              onChange={ handleChangeControlBtnClassName } />
             </div>
         </div>
     );
