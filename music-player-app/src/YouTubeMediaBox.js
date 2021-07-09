43c45bc70c179a8d93970e0acc9193a1673525e4
Attilius <leo20@freemail.hu>
2021-07-09 09:32:44 +0200

    Nested Youtube component with his params in YouTubeMediaBox component

--- music-player-app/src/YouTubeMediaBox.js
+++ music-player-app/src/YouTubeMediaBox.js
@@ -1,12 +1,22 @@
-import YoutubeApi from "./YoutubeApi";
+import YouTube from 'react-youtube';
 import TrackDatas from "./TrackDatas";
 
 
-function YouTubeMediaBox() {
+function YouTubeMediaBox({ youtubeId }) {
+    
+    const opts = {
+        height: '250',
+        width: '250',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
+      
     return(
         <div className="main">
             <div id="player">
-                <YoutubeApi />
+                <YouTube videoId={ youtubeId } opts={opts} />
             </div>
             <TrackDatas />
         </div>
