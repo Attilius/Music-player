dca12452ddbb6d557ca003a7d5ad520b225819eb
Attilius <leo20@freemail.hu>
2021-07-10 11:51:09 +0200

    Add a new method called onReady

--- music-player-app/src/YouTubeMediaBox.js
+++ music-player-app/src/YouTubeMediaBox.js
@@ -1,21 +1,32 @@
 import YouTube from 'react-youtube';
 import TrackDatas from "./TrackDatas";
 
-function YouTubeMediaBox({ youtubeId }) {
+function YouTubeMediaBox({ youtubeId, event_ }) {
+    
     const opts = {
         height: '250',
         width: '250',
         playerVars: {
           // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
-          controls: 0,
+          autoplay: 0,
+          controls: 1,
         },
-      };
+        onReady: (event) => {
+            if (event_ === "play") {
+                event.target.playVideo();
+            } else if (event_ === "stop") {
+                event.target.stopVideo();
+            } else if (event_ === "pause") {
+                event.target.pauseVideo();
+            }
+            
+        },
+    };
 
     return(
         <div className="main">
             <div id="player">
-                <YouTube videoId={ youtubeId } opts={ opts } />
+                <YouTube videoId={ youtubeId } opts={ opts } onReady={opts.onReady} />
             </div>
             <TrackDatas />
         </div>
